96c1020b55ba57ad7d8ea87b23269e832ba37087
KELVIN TINASHE CHADA <139448833+KTJETRO@users.noreply.github.com>
2025-11-18 12:28:26 +0200

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from supabase import create_client, Client
-from datetime import datetime
 
-# Read credentials from Streamlit secrets
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
